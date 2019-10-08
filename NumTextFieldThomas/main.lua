87445ca2c00ed3ecd024030453def570f59ba228
Thomas Enright <thomas.enright@ocsbstudent.ca>
2019-10-08 14:46:50 -0400

    Update main.lua

--- NumTextFieldThomas/main.lua
+++ NumTextFieldThomas/main.lua
@@ -8,6 +8,8 @@
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+-- sets the colour of the background
+display.setDefault("background", 255/255, 128/255, 0/255 )
 ------------------------------------------------------------------------------------
 -- Local variables
 --------------------------------------------------------------------------------------------
@@ -18,9 +20,15 @@ local correctObject
 local numericField
 local randomNumber1
 local randomNumber2
+local tempRandomNumber3
 local userAnswer
 local correctAnswer
 local incorrectAnswer
+local points = 0
+local lives = 3
+local livesObject
+local randomOperator
+
 
 -----------------------------------------------------------------------------------------------------------
 -- local Functions
@@ -28,18 +36,50 @@ local incorrectAnswer
 
 local function AskQuestion()
 	-- generate 2 random numbers between a max. and a min. number
-	randomNumber1 = math.random(0, 10)
-	randomNumber2 = math.random(0, 10)
+	randomNumber1 = math.random(1, 10)
+	randomNumber2 = math.random(1, 10)
 
-	correctAnswer = randomNumber1 + randomNumber2
+	-- chooses a random operator
+	randomOperator = math.random(1, 3)
 
-	-- create question in text object
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = " 
+	if (randomOperator == 1) then
+		correctAnswer = randomNumber1 + randomNumber2
 
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+	elseif (randomOperator == 2) then
+		
+		if (randomNumber1 < randomNumber2) then
+			tempRandomNumber3 = randomNumber1
+			randomNumber1 = randomNumber2
+			randomNumber2 = tempRandomNumber3
+		end
+
+		correctAnswer = randomNumber1 - randomNumber2
+
+		--create question in text object
+		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+		
+	
+	elseif (randomOperator == 3) then
+		correctAnswer = randomNumber1 * randomNumber2
+
+		--create question in text object
+		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+	end	
 end
+
+
+
 
 local function HideCorrect()
 	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function HideIncorrect()
+	IncorrectObject.isVisible = false
 	AskQuestion()
 end
 
@@ -49,18 +89,39 @@ local function numericFieldListener( event )
 	if ( event.phase == "began" ) then
 
 		-- clear text field
-		( eventTarget.text = "" )
+		event.target.text = " "
 
-	elseif event.phase == "submitted" then
+	elseif (event.phase == "submitted") then
+		userAnswer = tonumber(event.target.text)
 
 		-- when the answer is submitted (enter key is pressed) set user input to user's answer 
 		if (userAnswer == correctAnswer) then
+			IncorrectObject.isVisible = false
 			correctObject.isVisible = true
 			timer.performWithDelay(2000, HideCorrect)
+			event.target.text = " "
 
+		else 
+			IncorrectObject.isVisible = true
+			correctObject.isVisible = false
+			timer.performWithDelay(2000, HideIncorrect)
+			event.target.text = " "			
+			lives = lives - 1
+			livesObject.text = "Lives = " .. lives 
+			if (lives == 0) then
+				-- hide everything
+
+				-- display you lose
+			end
 		end
-	end
+	end	
 end
+
+
+
+
+
+
 
 ----------------------------------------------------------------------------------------------------------------------------
 -- object creation
@@ -68,12 +129,21 @@ end
 
 -- displays a question and sets the colour 
 questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
-questionObject:setTextColor(155/255, 42/255, 198/255)
+questionObject:setTextColor(0/255, 20/255, 250/255)
+
+-- displays the live count
+livesObject = display.newText("Lives = " .. lives, display.contentWidth/8, display.contentHeight/4, nil, 50 )
+questionObject:setTextColor(0/255, 20/255, 250/255)
 
 -- create the text and make it invisible
 correctObject = display.newText( "Correct", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
-correctObject:setTextColor(155/255, 42/255, 198/255)
+correctObject:setTextColor(0/255, 204/255, 0/255)
 correctObject.isVisible = false
+
+-- create the text and make it invisible
+IncorrectObject = display.newText( "Incorrect", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+IncorrectObject:setTextColor(255/255, 0/255, 0/255)
+IncorrectObject.isVisible = false
 
 -- create numeric field 
 numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
@@ -81,7 +151,6 @@ numericField.inputType = "number"
 
 -- add the event listener for the numeric field 
 numericField:addEventListener( "userInput", numericFieldListener )
-
 ---------------------------------------------------------------------------------------------------------------------------------------
 -- function calls
 --------------------------------------------------------------------------------------------------------
